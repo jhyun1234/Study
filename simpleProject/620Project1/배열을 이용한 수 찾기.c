046ec6986e2d4e3924ab9f6ec8f7f22f07f4eb6f
jhyun1234 <anjyounl24@gmail.com>
2024-04-15 14:46:39 +0900

    2024.4.15

---
+++ simpleProject/620Project1/배열을 이용한 수 찾기.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+#define SIZE 26
+int main()
+{
+	int total = 0; int target = 0;
+	int i = 0; int j = 0;
+	int sum = 0;
+	int s[100] = { 0, };
+	scanf("%d", &total);
+
+	for (i = 0; i < total; i++)
+	{
+		scanf("%d", &s[i]);
+	}
+
+	scanf("%d", &target);
+	for (j = 0; j < total; j++)
+	{
+		if (s[j] == target)
+		{
+			sum++;
+		}
+	}
+	printf("%d", sum);
+
+
+	
+	return 0;
+}
--- simpleProject/620Project1/소스.c
+++ simpleProject/620Project1/소스.c
@@ -1,10 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <math.h>
 
 int main()
 {
-
 
 
 
