27885ab34a2a2276d6e652c44c78166df9c95032
jhyun1234 <anjyounl24@gmail.com>
2024-04-30 11:51:36 +0900

    2024.4.30

--- simpleProject/620Project1/소스.c
+++ simpleProject/620Project1/소스.c
@@ -31,7 +31,7 @@ int main()
 		printf("%s\n", arr3);*/
 	
 
-
+	/*
 	// int 배열 크기는 10개 선언 --메모리 확보
 
 	int a[SIZE]; // 지역변수 - 쓰레기 값 -> 초기화를 하지않음
@@ -78,13 +78,60 @@ int main()
 	printf("\n");
 	printf("%d\n", max);
 	
-
+	*/
 		
 
+	/*
+	char a1[] = "Hello";
+	char a2[] = " World!";
+	char con[20];
+
+	int a = sizeof(a1) / sizeof(char);
+	printf("%d\n", a);
+	int b = sizeof(a2) / sizeof(char);
+	printf("%d\n", b);
+	int c = a + b;
+	printf("%d\n", c);
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < a-1; i++)
+	{
+		con[i] = a1[i];
+	}
+	/*for (j = a - 1; j < c - 1; j++) // c-1= 13 
+	{
+		con[j] = a2[j-a+1];  //  +1은 NULL문자를 넣어준것 
+		
+	}
 	
+	for (j = 0; j < b-1; j++)
+	{
+		con[i + j] = a2[j]; 
+	}
+	con[i + j] = '\0';
+
+	printf("%s", con);
+	*/
 
 
+    /**
+	int arr[3][5] = // 행은 적지않아도 되지만 열을 적지않으면 에러가 뜬다.
+	{
+		{1,2,3,4,5,},
+		{6,7,8,9,10},
+		{11,12,13,14,15}
+
+	};
 	
+	for (int i = 0; i < 3; i++) // 행
+	{
+		for (int j = 0; j < 5; j++) // 열
+		{
+			printf("%d ",arr[i][j]);
+		}
+		printf("\n");
+	}
+	*/
 
 	return 0;
 }
