9bf77cdd412dfb8ac897bca5c8a30171a6757f93
내 이름 <anjyounl24@gmail.com>
2024-05-22 00:01:48 +0900

    2024.5.22 집에서 포인터 공부

---
+++ simpleProject/620Project1/Pointer Home Study.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int num = 100, num1 = 200;
+	int* ptr;
+	ptr = &num;
+	(*ptr) += 30;
+	ptr = &num1;
+	(*ptr) -= 30;
+	printf("%d %d\n", num, num1);
+
+	return 0;
+}
